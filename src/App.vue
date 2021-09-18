8867c669745c0f964e04bca8e01bbac8b0d139a9
Elaine Rodrigues <elainepfarias@yahoo.com.br>
2021-09-17 23:59:21 -0400

    primeiro commit

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+
+<template>
+  <div>
+    <h1>Wise advice</h1>
+  </div>
+  <div>
+    <button class="bg-red-500" @click="getAdvice">Give me an advice</button>
+    <div>{{ this.advice }}</div>
+  </div>
+</template>
+
+<script>
+import axios from "axios"
+
+export default {
+  data() {
+        return {
+            advice: ""
+        }
+    },
+
+  methods: {
+    async getAdvice() {
+      const resp = await axios.get("https://api.adviceslip.com/advice");
+      this.advice = resp.data.slip.advice;
+    },
+
+  }
+
+}
+</script>
+
+<!-- <style>
+
+</style> -->
